30c9b1124fa87e36cda904aae5364d490177387f
sesuonap <tpanouses@gmail.com>
2018-05-10 21:34:17 -0500

    REST

--- app/controllers/api/recipes_controller.rb
+++
@@ -1,6 +0,0 @@
-class Api::RecipesController < ApplicationController
-  def one_recipe_action 
-    @recipe = Recipe.first
-    render 'one_recipe_view.json.jbuilder'
-  end 
-end
--- config/routes.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.routes.draw do
-  namespace :api do 
-    get '/one_recipe_url' => 'recipes#one_recipe_action'
-  end
-end 
